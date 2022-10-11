d27d67205c51da021958aecbfd279102a694f8d3
Frederic Lhoest <flhoest@gmail.com>
2022-10-11 14:12:19 +0200

    Update PhotoMeter.ino
    Bug fixes

--- PhotoMeter.ino
+++ PhotoMeter.ino
@@ -1,16 +1,24 @@
 /*
+   ______ __           __          _______         __              
+  |   __ \  |--.-----.|  |_.-----.|   |   |.-----.|  |_.-----.----.
+  |    __/     |  _  ||   _|  _  ||       ||  -__||   _|  -__|   _|
+  |___|  |__|__|_____||____|_____||__|_|__||_____||____|_____|__|  
+                                                     v 0.9              
+  (c) 2022 - Frederic Lhoest (and Anthony Guerrier)
+  
   Send solar power productionto Thingspeak
   ESP8266 + SCT-013 100A + Thingspeak
-  Mostly inspired by anthonyguerrier@free.fr from https://www.youtube.com/watch?v=smgw67uRQmw (sorry in French)
+  Mostly inspired by anthonyguerrier@free.fr from https://www.youtube.com/watch?v=smgw67uRQmw (in French)
 
   https://diyi0t.com/esp8266-wemos-d1-mini-tutorial/
+  https://siytek.com/guide-to-the-wemos-d1-mini/
 
-  PIN Reference for Wemos : 
+  WeMos D1 PIN naming convention : 
   static const uint8_t D0   = 16;
   static const uint8_t D1   = 5;
   static const uint8_t D2   = 4;
   static const uint8_t D3   = 0;
-  static const uint8_t D4   = 2;     <- never plug anything on this one it will crash it !
+  static const uint8_t D4   = 2;  
   static const uint8_t D5   = 14;
   static const uint8_t D6   = 12;
   static const uint8_t D7   = 13;
@@ -24,27 +32,32 @@
 #include <EmonLib.h>
 
 // Section related to LED (WIFI + DATA)
-int wifiPIN = 16;    //D0=16
-int dataPIN = 2;     //D4=2
+int wifiPIN = 16;    // pin D0=16
+int dataPIN = 2;     // pin D4=2
 
 // Current detected when no wire is enclosed by the clamp
 float ghostCurrent = 0.3;
 
 // Substitute by API Key write of your own thingspeak channel
-String apiKey = "DIP7U1R77ALBARFK";
+String apiKey = "<your_write_api_key";
 
-EnergyMonitor emon1;  // Create an instance
 
 // Substitute by your own Wifi SSID and password
 const char* ssid = "<your_ssid>";
 const char* password = "<your_password>";
-
 const char* server = "api.thingspeak.com";
+
+// Substitute by you local volatage : 110 for US 220 for rest of the world
+const int localVoltage = 220;
+
+// Create a energy monitoring instance
+EnergyMonitor emon1;  
 
 WiFiClient client;
 
 void setup() 
 {
+  double Irms;
 
   // Prepare LEDs for status display (DATA and WIFI)
   pinMode(wifiPIN, OUTPUT);       
@@ -52,9 +65,7 @@ void setup()
 
   pinMode(dataPIN, OUTPUT);       
   digitalWrite(dataPIN, LOW);  
-
-  double Irms;
-
+  
   Serial.begin(115200);
   delay(10);
   Serial.println("");
@@ -81,41 +92,41 @@ void setup()
   Serial.println("WiFi connceted!");
   Serial.print("IP address: "); Serial.println(WiFi.localIP()); 
   Serial.print("MAC: "); Serial.println(WiFi.macAddress());
+  // Display "Received Signal Level" - The lower, the best. -35 is much better than -90
   Serial.print("RSL: "); Serial.println(WiFi.RSSI());
 
   /* 
      The YHDC SCT-013-000 CT has 2000 turns, so the secondary peak current will be:
      Secondary peak-current = Primary peak-current / no. of turns = 141.4 A / 2000 = 0.0707A
-     Ideal burden resistance = (AREF/2) / Secondary peak-current = 2.5 V / 0.0707 A = 35.4 Î©
-     NB : Ce calcul pourrait Ãªtre modifiÃ© lors du montage rÃ©el, 22 ohms pour une carte en 3.3v.
+     Ideal burden resistance = (AREF/2) / Secondary peak-current = 2.5 V / 0.0707 A = 35.4 Ohm
+  */
 
-*/
-  emon1.current(0, 64.15);             // Current: input pin, calibration  => current constant = (68.6 Ã· 0.050) Ã· 33 = 41.6 @ Aref 3.2 Volts and R = 33
+  // emon1.current(0, 62);             
+  emon1.current(0, 64.15);             // Current: input pin, calibration  => current constant = (100 / 0.050) / 33 = 60.60
+
 /*
-
-From rectifier : 306 W
-Meseared from SCT-013 : 295 W
-
-306/295 -> 1.03
--> 62.282*1.03 = 64.15
-
-So far 64.15 is rhe best correction ratio according to my measures
-
- */
+  Inverter read : 306 W
+  Value measured : 295 W
+  
+  306 / 295 -> 1.03
+  -> 62.282 * 1.03 = 64.15
+  
+  So far 64.15 is rhe best correction ratio according to my measures
+*/
 
   // Init emon: run the routine 20 times to find the "0" level
   Serial.println(); Serial.print("Emon Initialisation ");
 
-  for (int i = 0; i < 10; i++) {        // LED flasing slow
-    digitalWrite(dataPIN, HIGH);        // LED on
+  for (int i = 0; i < 20; i++) {  // LED flasing slow
+    digitalWrite(dataPIN, HIGH); // LED on
     Serial.print(".");
-    Irms = emon1.calcIrms(1480);        // Calculate Irms only
-    delay(500);
+    Irms = emon1.calcIrms(1480);  // Calculate Irms only
+    delay(250);
 
-    digitalWrite(dataPIN, LOW);         // LED off
+    digitalWrite(dataPIN, LOW); // LED off
     Serial.print(".");
-    Irms = emon1.calcIrms(1480);        // Calculate Irms only
-    delay(500);
+    Irms = emon1.calcIrms(1480);  // Calculate Irms only
+    delay(250);
   }
   Serial.println(" Init done.");
 }
@@ -128,22 +139,28 @@ void loop()
 
     Serial.println("");
     Serial.println("Taking 40 samples and compute average");
-  
+    
+    /* Note : thingspeak requires a minimum of 15 secs between two data updates The logic below is sending
+     one measure every 20 secs, so we are good !
+     */
+     
     for (int i = 0; i < 40; i++) 
     {    
       // Compute Irms mean value over 20 seconds
-      tmp=emon1.calcIrms(1480);   // Calculate Irms only
-      // Deducting the measured current when no wire enclosed in the clamp
+      tmp=emon1.calcIrms(1480);   
+      // Deducting the measured current when no wire enclosed in the clamp - the ghost current
       tmp -= ghostCurrent;
       
       Irms += tmp;
-      Debug=emon1.calcIrms(1480);
+      Debug = tmp;
+      if(Debug < 0) Debug=0;
+      
       Serial.print("* DEBUG : Sample #");
       Serial.print(i);
       Serial.print(" -> ");
       Serial.print(Debug);
       Serial.print(" A. (");
-      Serial.print(abs(tmp * 220));
+      Serial.print(Debug * localVoltage);
       Serial.println(" Watt)");
       delay(500);
     }
@@ -151,11 +168,11 @@ void loop()
     // Averaging
     Irms /= 40;
     Serial.print("Average : ");
-    Serial.print(Irms);
+    Serial.print(abs(Irms));
     Serial.println(" A");
     
-    // If power value is below 30 W then consider there is no current -> 0 Watt    
-    if ((Irms*220) < 30) Irms = 0; 
+    // If power value is below 30 W then consider there is no real production -> 0 Watt    
+    if ((Irms * localVoltage) < 30) Irms = 0; 
 
     if (client.connect(server, 80)) 
     {
@@ -163,7 +180,7 @@ void loop()
       postStr += "&field1=";
       postStr += String(Irms);
       postStr += "&field2=";
-      postStr += String(Irms * 220);
+      postStr += String(Irms * localVoltage);
       postStr += "\r\n\r\n";
 
       client.print("POST /update HTTP/1.1\n");
@@ -176,12 +193,12 @@ void loop()
       client.print("\n\n");
       client.print(postStr);
 
-      // Blink DATA LED 8 times to confirm data sent
+      // Blink DATA LED 8 times to confirm data has been sent
       for (int l = 0; l< 8; l++)
       {
-        digitalWrite(dataPIN, HIGH);      // LED off
+        digitalWrite(dataPIN, HIGH);  // LED off
         delay(80);
-        digitalWrite(dataPIN, LOW);       // LED on 
+        digitalWrite(dataPIN, LOW);  // LED on 
         delay(80);
       }
     
@@ -189,14 +206,12 @@ void loop()
       Serial.print("*** Current: ");
       Serial.print(Irms);
       Serial.print(" A. Power: ");
-      Serial.print(Irms * 220);
+      Serial.print(Irms * localVoltage);
       Serial.println(" W (sent to ThingSpeak).");
-
   }
   client.stop();
 
   Serial.println("Next data reading in 20 secs...");
 
-  // delay repaced by Irms mean value computed above
-  //  delay(20000);  
+  //  delay(20000);  // delay replaced by Irms mean value computed above
 }
